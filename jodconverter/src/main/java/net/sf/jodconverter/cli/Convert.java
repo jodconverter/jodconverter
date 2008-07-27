69ed53299304498cf9d20d672752a88b4748b916
mirko.nasato <mirko.nasato@e8ee3d98-163c-0410-9f04-db2354e518cf>
2008-07-27 14:17:34 +0000

    added --output-format option to command line tool

--- jodconverter/src/main/java/net/sf/jodconverter/cli/Convert.java
+++ jodconverter/src/main/java/net/sf/jodconverter/cli/Convert.java
@@ -2,6 +2,15 @@ package net.sf.jodconverter.cli;
 
 import java.io.File;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.apache.commons.io.FilenameUtils;
+
 import net.sf.jodconverter.OfficeDocumentConverter;
 import net.sf.jodconverter.office.ManagedProcessOfficeManager;
 import net.sf.jodconverter.office.OfficeManager;
@@ -13,26 +22,51 @@ public class Convert {
 
     public static final int STATUS_OK = 0;
     public static final int STATUS_MISSING_INPUT_FILE = 1;
-    public static final int STATUS_INVALID_ARG_COUNT = 255;
+    public static final int STATUS_INVALID_ARGUMENTS = 255;
 
-    public static void main(String[] args) {
-        if (args.length != 2) {
-            System.out.println("USAGE: convert input-file output-file");
-            System.exit(STATUS_INVALID_ARG_COUNT);
+    private static final Option OPTION_OUTPUT_FORMAT = new Option("f", "output-format", true, "output format (e.g. pdf)");
+    private static final Options OPTIONS = initOptions();
+
+    private static Options initOptions() {
+        Options options = new Options();
+        options.addOption(OPTION_OUTPUT_FORMAT);
+        return options;
+    }
+
+    public static void main(String[] arguments) throws ParseException {
+        CommandLineParser commandLineParser = new PosixParser();
+        CommandLine commandLine = commandLineParser.parse(OPTIONS, arguments);
+
+        String outputFormat = null;
+        if (commandLine.hasOption(OPTION_OUTPUT_FORMAT.getOpt())) {
+            outputFormat = commandLine.getOptionValue(OPTION_OUTPUT_FORMAT.getOpt());
         }
-        
-        File inputFile = new File(args[0]);
-        File outputFile = new File(args[1]);
-        if (!inputFile.isFile()) {
-            System.err.println("ERROR! No such file: " + inputFile);
-            System.exit(STATUS_MISSING_INPUT_FILE);
+
+        String[] fileNames = commandLine.getArgs();
+        if ((outputFormat == null && fileNames.length != 2) || fileNames.length < 1) {
+            String syntax = "java net.sf.jodconverter.cli.Convert [options] input-file output-file; or\n"
+                    + "[options] -f output-format input-file [input-file...]";
+            HelpFormatter helpFormatter = new HelpFormatter();
+            helpFormatter.printHelp(syntax, OPTIONS);
+            System.exit(STATUS_INVALID_ARGUMENTS);
         }
         
         OfficeManager officeManager = getOfficeManager();
         officeManager.start();
         OfficeDocumentConverter converter = new OfficeDocumentConverter(officeManager);
         try {
-            converter.convert(inputFile, outputFile);
+            if (outputFormat == null) {
+                File inputFile = new File(fileNames[0]);
+                File outputFile = new File(fileNames[1]);
+                converter.convert(inputFile, outputFile);
+            } else {
+                for (int i = 0; i < fileNames.length; i++) {
+                    File inputFile = new File(fileNames[i]);
+                    String outputName = FilenameUtils.getBaseName(fileNames[i]) + "." + outputFormat;
+                    File outputFile = new File(FilenameUtils.getFullPath(fileNames[i]) + outputName);
+                    converter.convert(inputFile, outputFile);
+                }
+            }
         } finally {
             officeManager.stop();
         }
