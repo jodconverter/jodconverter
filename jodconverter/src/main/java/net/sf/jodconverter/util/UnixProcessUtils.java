18c06f0fa8914450e4b8181e746026e5ac97441e
mirko.nasato <mirko.nasato@e8ee3d98-163c-0410-9f04-db2354e518cf>
2009-01-04 23:06:36 +0000

    updated licence to LGPL v3

--- jodconverter/src/main/java/net/sf/jodconverter/util/UnixProcessUtils.java
+++ jodconverter/src/main/java/net/sf/jodconverter/util/UnixProcessUtils.java
@@ -1,20 +1,20 @@
 //
 // JODConverter - Java OpenDocument Converter
-// Copyright (C) 2004-2008 - Mirko Nasato <mirko@artofsolving.com>
+// Copyright (C) 2004-2009 - Mirko Nasato and Contributors
 //
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
+// JODConverter is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
 //
-// This library is distributed in the hope that it will be useful,
+// JODConverter is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, you can find it online
-// at http://www.gnu.org/licenses/lgpl-2.1.html.
+// You should have received a copy of the GNU Lesser General
+// Public License along with JODConverter.  If not, see
+// <http://www.gnu.org/licenses/>.
 //
 package net.sf.jodconverter.util;
 
--- jodconverter/src/test/java/net/sf/jodconverter/test/TestUtils.java
+++ jodconverter/src/test/java/net/sf/jodconverter/test/TestUtils.java
@@ -1,3 +1,21 @@
+//
+// JODConverter - Java OpenDocument Converter
+// Copyright (C) 2004-2009 - Mirko Nasato and Contributors
+//
+// JODConverter is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// JODConverter is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General
+// Public License along with JODConverter.  If not, see
+// <http://www.gnu.org/licenses/>.
+//
 package net.sf.jodconverter.test;
 
 import java.io.File;
