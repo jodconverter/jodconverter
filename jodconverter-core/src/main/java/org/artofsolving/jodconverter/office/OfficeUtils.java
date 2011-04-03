8dbecf9f354786acca00c4eb094b80b62d1328ec
mirko.nasato <mirko.nasato@e8ee3d98-163c-0410-9f04-db2354e518cf>
2011-04-03 13:43:52 +0000

    updated copyright headers

--- jodconverter-core/src/main/java/org/artofsolving/jodconverter/office/OfficeUtils.java
+++ jodconverter-core/src/main/java/org/artofsolving/jodconverter/office/OfficeUtils.java
@@ -1,7 +1,6 @@
 //
 // JODConverter - Java OpenDocument Converter
-// Copyright 2009 Art of Solving Ltd
-// Copyright 2004-2009 Mirko Nasato
+// Copyright 2004-2011 Mirko Nasato and contributors
 //
 // JODConverter is free software: you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public License
--- jodconverter-core/src/main/java/org/artofsolving/jodconverter/process/ProcessQuery.java
+++ jodconverter-core/src/main/java/org/artofsolving/jodconverter/process/ProcessQuery.java
@@ -1,3 +1,21 @@
+//
+// JODConverter - Java OpenDocument Converter
+// Copyright 2004-2011 Mirko Nasato and contributors
+//
+// JODConverter is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// JODConverter is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General
+// Public License along with JODConverter.  If not, see
+// <http://www.gnu.org/licenses/>.
+//
 package org.artofsolving.jodconverter.process;
 
 public class ProcessQuery {
