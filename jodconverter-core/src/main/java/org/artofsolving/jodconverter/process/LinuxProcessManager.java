6bf6527fa9264267ab88397bf79b44ddfdae3797
mirko.nasato@gmail.com <mirko.nasato@gmail.com@e8ee3d98-163c-0410-9f04-db2354e518cf>
2011-12-07 20:26:43 +0000

    issue #101 - avoid killing unintended processes in LinuxProcessManager if the office pid wasn't found

--- jodconverter-core/src/main/java/org/artofsolving/jodconverter/process/LinuxProcessManager.java
+++ jodconverter-core/src/main/java/org/artofsolving/jodconverter/process/LinuxProcessManager.java
@@ -64,6 +64,9 @@ public class LinuxProcessManager implements ProcessManager {
     }
 
     public void kill(Process process, long pid) throws IOException {
+    	if (pid <= 0) {
+    		throw new IllegalArgumentException("invalid pid: " + pid);
+    	}
         execute("/bin/kill", "-KILL", Long.toString(pid));
     }
 
