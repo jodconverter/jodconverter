c093f27b29c2d44ef8b6175d24b07d271cb574d7
Simon Braconnier <simonbraconnier@gmail.com>
2016-11-22 00:56:49 -0500

    Code formatting

--- jodconverter-spring/src/test/java/org/artofsolving/jodconverter/spring/SpringControllerTest.java
+++ jodconverter-spring/src/test/java/org/artofsolving/jodconverter/spring/SpringControllerTest.java
@@ -1,8 +1,13 @@
 package org.artofsolving.jodconverter.spring;
 
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
 import java.io.PrintWriter;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,81 +27,79 @@ public class SpringControllerTest {
 
         // this bean will be injected into the OrderServiceTest class
         @Bean
-        public SpringJodConverter springJoDConverter() {
-            SpringJodConverter sjd = new SpringJodConverter();
+        public JodConverterBean springJoDConverter() {
+            JodConverterBean sjd = new JodConverterBean();
             // set properties, etc.
             return sjd;
         }
     }
 
-    File TXTinputFile = null;
-    File RTFoutputFile = null;
-    File DOCoutputFile = null;
-    File PDFoutputFile = null;
-    File DOCXoutputFile = null;
+
+    private File inputFileTXT = null;
+    private File outputFileRTF = null;
+    private File outputFileDOC = null;
+    private File outputFilePDF = null;
+    private File outputFileDOCX = null;
 
     @Autowired
-    private SpringJodConverter testClass = null;
+    private JodConverterBean bean = null;
 
     @Before
     public void setUp() throws Exception {
-        TXTinputFile = new File("input.txt");
-        if (!TXTinputFile.exists()) {
-            boolean b = TXTinputFile.createNewFile();
-            if (!b)
-                throw new Exception("Cannot create the input file.");
-        } else {
-            PrintWriter pw = new PrintWriter(TXTinputFile);
+
+        inputFileTXT = File.createTempFile("JodConverterTest", ".txt");
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter(inputFileTXT);
             pw.println("This is the first line of the input file.");
             pw.println("This is the second line of the input file.");
-            pw.close();
+        } catch (Exception e) {
+            IOUtils.closeQuietly(pw);
         }
-        RTFoutputFile = new File("output.rtf");
-        DOCoutputFile = new File("output.doc");
-        PDFoutputFile = new File("output.pdf");
-        DOCXoutputFile = new File("output.DOCX");
+
+        File parent = inputFileTXT.getParentFile();
+        String basename = FilenameUtils.getBaseName(inputFileTXT.getName());
+        outputFileRTF = new File(parent, basename + ".rtf");
+        outputFileDOC = new File(parent, basename + ".doc");
+        outputFilePDF = new File(parent, basename + ".pdf");
+        outputFileDOCX = new File(parent, basename + ".docx");
     }
 
     @After
     public void tearDown() throws Exception {
-        TXTinputFile.delete();
-        RTFoutputFile.delete();
-        DOCoutputFile.delete();
-        PDFoutputFile.delete();
-        DOCXoutputFile.delete();
-    }
 
-    @Test
-    public void testAvailableFormats() throws Exception {
-        testClass.availableFormats();
+        FileUtils.deleteQuietly(inputFileTXT);
+        FileUtils.deleteQuietly(outputFileRTF);
+        FileUtils.deleteQuietly(outputFileDOC);
+        FileUtils.deleteQuietly(outputFilePDF);
+        FileUtils.deleteQuietly(outputFileDOCX);
     }
 
     @Test
     public void testTXTToRTF() throws Exception {
-        testClass.convert(TXTinputFile.toString(), RTFoutputFile.toString());
-        if (!RTFoutputFile.exists())
-            throw new Exception("There is not a RTF File!.");
+
+        bean.convert(inputFileTXT, outputFileRTF);
+        assertTrue("RTF File not created.", outputFileRTF.exists());
     }
 
     @Test
     public void testTXTToDOC() throws Exception {
-        testClass.convert(TXTinputFile.toString(), DOCoutputFile.toString());
-        if (!DOCoutputFile.exists())
-            throw new Exception("There is not a DOC File!.");
+
+        bean.convert(inputFileTXT, outputFileDOC);
+        assertTrue("DOC File not created.", outputFileDOC.exists());
     }
 
     @Test
     public void testTXTToDOCX() throws Exception {
-        testClass.convert(TXTinputFile.toString(), DOCXoutputFile.toString());
-        if (!DOCXoutputFile.exists())
-            throw new Exception("There is not a DOCX File!.");
+
+        bean.convert(inputFileTXT, outputFileDOCX);
+        assertTrue("DOCX File not created.", outputFileDOCX.exists());
     }
 
     @Test
     public void testTXTToPDF() throws Exception {
-        testClass.convert(TXTinputFile.toString(), PDFoutputFile.toString());
-        if (!PDFoutputFile.exists())
-            throw new Exception("There is not a PDF File!.");
-    }
 
+        bean.convert(inputFileTXT, outputFilePDF);
+        assertTrue("PDF File not created.", outputFilePDF.exists());
+    }
 }
