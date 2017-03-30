1f3a2687aa1e5adfbc557cba0809959d46e27508
Simon Braconnier <simonbraconnier@gmail.com>
2017-03-30 18:00:52 -0400

    Create a filter that allow text replacement while converting a document.

---
+++ jodconverter-core/src/main/java/org/artofsolving/jodconverter/filter/TextReplacerFilter.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2004 - 2012 Mirko Nasato and contributors
+ *           2016 - 2017 Simon Braconnier and contributors
+ *
+ * This file is part of JODConverter - Java OpenDocument Converter.
+ *
+ * JODConverter is an Open Source software: you can redistribute it and/or
+ * modify it under the terms of either (at your option) of the following
+ * licenses:
+ *
+ * 1. The GNU Lesser General Public License v3 (or later)
+ *    http://www.gnu.org/licenses/lgpl-3.0.txt
+ * 2. The Apache License, Version 2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+
+package org.artofsolving.jodconverter.filter;
+
+import org.apache.commons.lang3.Validate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sun.star.lang.XComponent;
+import com.sun.star.uno.UnoRuntime;
+import com.sun.star.util.XReplaceDescriptor;
+import com.sun.star.util.XReplaceable;
+
+import org.artofsolving.jodconverter.office.OfficeContext;
+import org.artofsolving.jodconverter.office.OfficeException;
+
+/** This filter is used to replace text in a document. */
+public class TextReplacerFilter implements Filter {
+
+  // This class has been inspired by this example:
+  // http://api.libreoffice.org/examples/java/Text/TextReplace.java
+
+  private static final Logger logger = LoggerFactory.getLogger(TextReplacerFilter.class);
+
+  private final String[] searchList;
+  private final String[] replacementList;
+
+  /**
+   * Creates a new filter with the specified strings to replace.
+   *
+   * @param searchList the Strings to search for, no-op if null.
+   * @param replacementList the Strings to replace them with, no-op if null.
+   */
+  public TextReplacerFilter(final String[] searchList, final String[] replacementList) {
+    super();
+
+    // Both arrays are required and cannot be empty
+    Validate.notEmpty(searchList, "Search list is empty");
+    Validate.notEmpty(replacementList, "Replacement list is empty");
+
+    // Make sure lengths are ok, these need to be equal
+    final int searchLength = searchList.length;
+    final int replacementLength = replacementList.length;
+    Validate.isTrue(
+        searchList.length == replacementList.length,
+        "Search and Replace array lengths don't match: "
+            + searchLength
+            + " vs "
+            + replacementLength);
+
+    // Everything is fine
+    this.searchList = searchList;
+    this.replacementList = replacementList;
+  }
+
+  @Override
+  public void doFilter(
+      final OfficeContext context, final XComponent document, final FilterChain chain)
+      throws OfficeException {
+
+    try {
+      final XReplaceable replaceable = UnoRuntime.queryInterface(XReplaceable.class, document);
+
+      // We need a descriptor to set properties for Replace
+      final XReplaceDescriptor replaceDescr = replaceable.createReplaceDescriptor();
+
+      logger.debug("Change all occurrences of ...");
+      for (int i = 0; i < searchList.length; i++) {
+        logger.debug(searchList[i] + " -> " + replacementList[i]);
+        // Set the properties the replace method need
+        replaceDescr.setSearchString(searchList[i]);
+        replaceDescr.setReplaceString(replacementList[i]);
+
+        // Replace all words
+        replaceable.replaceAll(replaceDescr);
+      }
+
+    } catch (Exception e) {
+      logger.error("Error", e);
+    }
+
+    chain.doFilter(context, document);
+  }
+}
---
+++ jodconverter-core/src/test/java/org/artofsolving/jodconverter/filter/TextReplacerFilterTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2004 - 2012 Mirko Nasato and contributors
+ *           2016 - 2017 Simon Braconnier and contributors
+ *
+ * This file is part of JODConverter - Java OpenDocument Converter.
+ *
+ * JODConverter is an Open Source software: you can redistribute it and/or
+ * modify it under the terms of either (at your option) of the following
+ * licenses:
+ *
+ * 1. The GNU Lesser General Public License v3 (or later)
+ *    http://www.gnu.org/licenses/lgpl-3.0.txt
+ * 2. The Apache License, Version 2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+
+package org.artofsolving.jodconverter.filter;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import org.artofsolving.jodconverter.office.OfficeException;
+
+public class TextReplacerFilterTest extends FilterTest {
+
+  private static final String SOURCE_FILE = "src/test/resources/documents/test_replace.doc";
+  private static final String OUTPUT_DIR =
+      "test-output/" + TextReplacerFilterTest.class.getSimpleName();
+
+  /** Clears the output directory before the execution of the first test in this class. */
+  @BeforeClass
+  public static void deleteOutputDirectory() {
+
+    final File testOutputDir = new File(OUTPUT_DIR);
+    FileUtils.deleteQuietly(testOutputDir);
+  }
+
+  /**
+   * Test the conversion of a document replacing text along the way.
+   *
+   * @throws OfficeException if an error occurs.
+   */
+  @Test
+  public void doFilter_WithDefaultProperties() throws OfficeException {
+
+    final File sourceFile = new File(SOURCE_FILE);
+    final File testOutputDir = new File(OUTPUT_DIR);
+
+    // Create the GraphicInserterFilter to test.
+    final TextReplacerFilter filter =
+        new TextReplacerFilter(
+            new String[] {"SEARCH_WORD", "that", "have", "new common language will be more simple"},
+            new String[] {
+              "REPLACEMENT_STRING",
+              "REPLACEMENT_THAT",
+              "REPLACEMENT_HAVE",
+              "most recent common language will be more basic"
+            });
+
+    // Test the filter
+    testFilters(sourceFile, testOutputDir, "test", filter, RefreshFilter.INSTANCE);
+  }
+}
