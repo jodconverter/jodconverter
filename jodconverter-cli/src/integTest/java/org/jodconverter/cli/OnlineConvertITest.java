90dc1dacc139606b4e987dee622a0541f567b4a0
Simon Braconnier <simonbraconnier@gmail.com>
2017-09-12 00:28:00 -0400

    monir refactoring

--- jodconverter-cli/src/integTest/java/org/jodconverter/cli/OnlineConvertITest.java
+++ jodconverter-cli/src/integTest/java/org/jodconverter/cli/OnlineConvertITest.java
@@ -42,7 +42,6 @@ import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import org.jodconverter.cli.util.ExitException;
 import org.jodconverter.cli.util.NoExitResource;
 import org.jodconverter.cli.util.ResetExitExceptionResource;
-import org.jodconverter.cli.util.SystemLogHandler;
 
 /**
  * This class tests the {@link Convert} class, which contains the main function of the cli module.
@@ -76,7 +75,6 @@ public class OnlineConvertITest {
 
     try {
 
-      SystemLogHandler.startCapture();
       Convert.main(
           new String[] {
             inputFileTmp.getPath(), "-f", "txt", "-c", "http://localhost:9980/lool/convert-to/"
@@ -87,7 +85,6 @@ public class OnlineConvertITest {
 
     } catch (Exception ex) {
       try {
-        SystemLogHandler.stopCapture();
         assertThat(ex)
             .isExactlyInstanceOf(ExitException.class)
             .hasFieldOrPropertyWithValue("status", 0);
