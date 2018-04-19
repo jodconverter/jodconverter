e6b77c87def580db4cfe9feb57feb2b2494e163c
Liu Rui <lovearuis@gmail.com>
2018-04-19 11:30:04 +0800

    code style

--- jodconverter-local/src/main/java/org/jodconverter/task/AbstractLocalOfficeTask.java
+++ jodconverter-local/src/main/java/org/jodconverter/task/AbstractLocalOfficeTask.java
@@ -88,8 +88,8 @@ public abstract class AbstractLocalOfficeTask extends AbstractOfficeTask {
     final Map<String, Object> loadProps =
         new HashMap<>(
             Optional.ofNullable(loadProperties).orElse(LocalConverter.DEFAULT_LOAD_PROPERTIES));
-    if (source.getFormat() !=null) {
-        appendProperties(loadProps, source.getFormat().getLoadProperties());
+    if (source.getFormat() != null) {
+      appendProperties(loadProps, source.getFormat().getLoadProperties());
     }
 
     return loadProps;
