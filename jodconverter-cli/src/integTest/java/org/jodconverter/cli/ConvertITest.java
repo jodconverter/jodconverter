jodconverter/jodconverter

commit 5c5ad5386feb4419c89c84314208c12a233ec4de

code style

jodconverter-core/src/main/java/org/jodconverter/job/AbstractConversionJobWithSourceFormatUnspecified.java
@@ -23,8 +23,8 @@ import java.io.File;
 import java.io.OutputStream;
 
 import org.apache.commons.io.FilenameUtils;
-
 import org.apache.commons.lang3.Validate;
+
 import org.jodconverter.document.DocumentFormat;
 import org.jodconverter.document.DocumentFormatRegistry;
 import org.jodconverter.office.OfficeManager;

commit 3d76ed5292aa7107f5b43e4ce0e208a926ba499b

Fix the bug introduced by #65 when source is an InputStream.

jodconverter-cli/src/integTest/java/org/jodconverter/cli/ConvertITest.java
@@ -56,11 +56,11 @@ public class ConvertITest {
   @Rule public ResetExitExceptionResource resetExitEx = new ResetExitExceptionResource();
 
   @Test
-  public void convert_WithCustomFormatRegistry_ShouldSupportOnlyTxtToPdf() throws Exception {
+  public void convert_WithCustomFormatRegistry_ShouldSupportOnlyTargetTxtOrPdf() throws Exception {
 
     final File registryFile = new File(CONFIG_DIR + "cli-document-formats.json");
     final File inputFile = new File(SOURCE_FILE);
-    final File outputFile = new File(testFolder.getRoot(), "convert_WithMultipleFilters.pdf");
+    final File outputFile = new File(testFolder.getRoot(), "convert_WithMultipleFilters.doc");
 
     assertThat(outputFile).doesNotExist();
 
@@ -80,7 +80,7 @@ public class ConvertITest {
           .isExactlyInstanceOf(ExitException.class)
           .hasFieldOrPropertyWithValue("status", 2);
 
-      assertThat(capturedlog).contains("The source format is missing or not supported");
+      assertThat(capturedlog).contains("The target format is missing or not supported");
     }
   }
 

jodconverter-core/src/main/java/org/jodconverter/job/AbstractConversionJobWithSourceFormatUnspecified.java
@@ -23,7 +23,6 @@ import java.io.File;
 import java.io.OutputStream;
 
 import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.lang3.Validate;
 
 import org.jodconverter.document.DocumentFormat;
 import org.jodconverter.document.DocumentFormatRegistry;
@@ -106,11 +105,8 @@ public abstract class AbstractConversionJobWithSourceFormatUnspecified
 
   private AbstractConversionJob toInternal(final AbstractTargetDocumentSpecs target) {
 
-    // Validate that we know the format, but only if the extension was provided.
-    // For a source file, we can let OOo guess what is the type of the source file.
-    if (!FilenameUtils.getExtension(source.getFile().getName()).isEmpty()) {
-      Validate.notNull(source.getFormat(), "The source format is missing or not supported");
-    }
+    // No need to validate that the source format is provided. We will let
+    // OOo deal with the detection of the source file format.
 
     return to(target);
   }

jodconverter-local/src/test/java/org/jodconverter/LocalConverterTest.java
@@ -47,8 +47,6 @@ import org.jodconverter.task.LocalConversionTask;
 public class LocalConverterTest {
 
   private static final File SOURCE_FILE = new File("src/test/resources/documents/test.txt");
-  private static final File BAD_SOURCE_FILE =
-      new File("src/test/resources/documents/test.unsupportedext");
 
   @ClassRule public static TemporaryFolder testFolder = new TemporaryFolder();
   private static File outputDir;
@@ -80,14 +78,6 @@ public class LocalConverterTest {
     final File targetFile = new File(outputDir, "test.pdf");
 
     LocalConverter.make().convert(SOURCE_FILE).to(targetFile).execute();
-  }
-
-  @Test(expected = NullPointerException.class)
-  public void convert_WithoutSourceFileFormat_ThrowsNullPointerException() throws Exception {
-
-    final File targetFile = new File(outputDir, "test.pdf");
-
-    LocalConverter.make(officeManager).convert(BAD_SOURCE_FILE).to(targetFile).execute();
   }
 
   @Test

jodconverter-online/src/test/java/org/jodconverter/OnlineConverterTest.java
@@ -39,8 +39,6 @@ import org.jodconverter.office.OfficeManager;
 public class OnlineConverterTest {
 
   private static final File SOURCE_FILE = new File("src/test/resources/documents/test.txt");
-  private static final File BAD_SOURCE_FILE =
-      new File("src/test/resources/documents/test.unsupportedext");
 
   @ClassRule public static TemporaryFolder testFolder = new TemporaryFolder();
   private static File outputDir;
@@ -72,14 +70,6 @@ public class OnlineConverterTest {
     final File targetFile = new File(outputDir, "test.pdf");
 
     OnlineConverter.make().convert(SOURCE_FILE).to(targetFile).execute();
-  }
-
-  @Test(expected = NullPointerException.class)
-  public void convert_WithoutSourceFileFormat_ThrowsNullPointerException() throws Exception {
-
-    final File targetFile = new File(outputDir, "test.pdf");
-
-    OnlineConverter.make(officeManager).convert(BAD_SOURCE_FILE).to(targetFile).execute();
   }
 
   @Test