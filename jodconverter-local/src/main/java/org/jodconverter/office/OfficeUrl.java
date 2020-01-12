0b4e4940ecf1e91a2ea4bbf0e6bfe1d41b410200
sbraconnier <simonbraconnier@gmail.com>
2020-01-12 12:56:05 -0500

    Try to fix the build under FreeBSD

--- jodconverter-local/src/main/java/org/jodconverter/office/OfficeUrl.java
+++ jodconverter-local/src/main/java/org/jodconverter/office/OfficeUrl.java
@@ -72,8 +72,11 @@ public class OfficeUrl {
     // Here we must use a try catch since OpenOffice and LibreOffice doesn't
     // have the same UnoUrl.parseUnoUrl signature
     try {
+      //      return UnoUrl.parseUnoUrl(
+      //          "socket,host=127.0.0.1,port=" + port +
+      // ",tcpNoDelay=1;urp;StarOffice.ServiceManager");
       return UnoUrl.parseUnoUrl(
-          "socket,host=127.0.0.1,port=" + port + ",tcpNoDelay=1;urp;StarOffice.ServiceManager");
+          "socket,host=localhost,port=" + port + ";urp;StarOffice.ServiceManager");
     } catch (Exception ex) {
       throw new IllegalArgumentException(ex);
     }
