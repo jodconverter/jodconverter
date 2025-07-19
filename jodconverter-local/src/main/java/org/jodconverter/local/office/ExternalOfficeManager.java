55ba7d3fb32bfec278b5b9bfd18a95a0ffd78f2b
sbraconnier <simonbraconnier@gmail.com>
2025-07-19 09:27:01 -0400

    Polish

--- jodconverter-local/src/main/java/org/jodconverter/local/office/ExternalOfficeManager.java
+++ jodconverter-local/src/main/java/org/jodconverter/local/office/ExternalOfficeManager.java
@@ -21,9 +21,6 @@
 package org.jodconverter.local.office;
 
 import java.io.File;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -33,7 +30,6 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 
 import org.jodconverter.core.office.AbstractOfficeManagerPool;
 import org.jodconverter.core.office.InstalledOfficeManagerHolder;
-import org.jodconverter.core.office.OfficeException;
 import org.jodconverter.core.office.OfficeUtils;
 import org.jodconverter.core.task.OfficeTask;
 import org.jodconverter.core.util.AssertUtils;
@@ -59,14 +55,6 @@ import org.jodconverter.core.util.AssertUtils;
  */
 public final class ExternalOfficeManager
     extends AbstractOfficeManagerPool<ExternalOfficeManagerPoolEntry> {
-
-  public static void main(String[] args) throws UnsupportedEncodingException, OfficeException {
-    ExternalOfficeManager.Builder config = ExternalOfficeManager.builder();
-    String wopiURI = "http://localhost:9980/wopi/files/some-sample-document.odt";
-    String encodedURI = URLEncoder.encode(wopiURI, String.valueOf(StandardCharsets.UTF_8));
-    ExternalOfficeManager officeManager = config.websocketUrls("ws://localhost:9980/cool/" + encodedURI + "/ws").build();
-    officeManager.stop();
-  }
 
   // The default value for hostName.
   /* default */ static final String DEFAULT_HOSTNAME = "127.0.0.1";
